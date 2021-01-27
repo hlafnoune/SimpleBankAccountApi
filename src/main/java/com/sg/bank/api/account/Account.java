bd90c391d94a4f1b2d874dfa72f1b7fc6ed308b7
LAFNOUNE Hicham <lafnoune.hicham@gmail.com>
2021-01-27 23:06:52 +0100

    US1 : make a deposit in an account

--- src/main/java/com/sg/bank/api/account/Account.java
+++ src/main/java/com/sg/bank/api/account/Account.java
@@ -1,4 +1,21 @@
 package com.sg.bank.api.account;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class Account {
+
+    private BigDecimal balance;
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void depoit(BigDecimal amount) {
+        balance = Optional.ofNullable(balance).orElse(BigDecimal.ZERO).add(amount);
+    }
 }
--- src/test/java/com/sg/bank/api/account/AccountTest.java
+++ src/test/java/com/sg/bank/api/account/AccountTest.java
@@ -1,8 +1,33 @@
 package com.sg.bank.api.account;
 
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AccountTest {
 
+    private Account account = new Account();
+
+    @Nested
+    class deposit {
+
+        @Test
+        void should_make_deposit_in_the_account_when_the_initial_balance_is_null() {
+            account.depoit(BigDecimal.valueOf(100));
+            assertThat(account.getBalance()).isEqualTo(BigDecimal.valueOf(100));
+        }
+
+        @Test
+        void should_make_deposit_in_the_account_and_add_the_amount_to_the_existing_balance() {
+            account.setBalance(BigDecimal.valueOf(300));
+            account.depoit(BigDecimal.valueOf(100));
+            assertThat(account.getBalance()).isEqualTo(BigDecimal.valueOf(400));
+        }
+    }
 
 
 }
